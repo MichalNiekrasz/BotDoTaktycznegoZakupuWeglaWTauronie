dee67c30fe844c2835fd8caa5514a67b6dee6a84
Michał Niekrasz <mnierasz@gmail.com>
2022-06-11 22:21:38 +0200

    init

---
+++ bot.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+import time
+c = "C:/Users/mnier/Downloads/chromedriver_win32/chromedriver.exe"
+window = webdriver.Chrome(c)
+
+window.get("https://sklep.tauron.pl")
+
+
+#C:\Users\mnier\Downloads\chromedriver_win32\chromedriver.exe
